3e464f49e82946f2677ebe246134b0b8a554b301
Charles Park <47514859+charlespark4393@users.noreply.github.com>
2023-12-14 17:31:33 -0500

    contact info upgrade

--- src/pages/contact/ContactInfo.js
+++ src/pages/contact/ContactInfo.js
@@ -7,8 +7,6 @@ const ContactInfo = (props) => {
         <div className={contactBoxClass ? contactBoxClass : 'ras-contact-address'}>
             <Fade left>
                 <h2>GET IN TOUCH</h2>
-                <p>With a focus on your project's success, our team advocates top software development firms, evaluating rates alongside deep expertise.</p>
-
                 <h6 className="mb-0">Address:</h6>
                 <p className="mb-10">32-26 168th St Flushing, NY 11358</p>
                 <h6 className="mb-0">Email:</h6>
